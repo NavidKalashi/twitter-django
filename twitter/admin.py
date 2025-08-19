714d9f5a02b1c22e98294420193237ca1595f1ed
Navid Kalashi <kalashinavid@gmail.com>
2025-08-19 14:53:03 +0330

    cusomizing admin 2

--- twitter/admin.py
+++ twitter/admin.py
@@ -3,10 +3,18 @@ from . import models
 
 @admin.register(models.Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ['author', 'text']
-    list_per_page = 5
+    list_display = ['short_text']
+    list_per_page = 10
+    list_select_related = ['author']
+    list_filter = ['updated_at']
+
+    def short_text(self, tweet):
+        return (tweet.text[:50] + '...    |  ' + tweet.author.username if len(tweet.text) > 50 else tweet.text)
+    
+    short_text.short_description = "text"
 
 @admin.register(models.CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'bio']
     list_per_page = 10
+    search_fields = ['username__istartswith']

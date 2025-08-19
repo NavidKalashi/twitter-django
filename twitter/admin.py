4909f5d427516d639e19e26082ea06c049e6e70a
Navid Kalashi <kalashinavid@gmail.com>
2025-08-19 12:35:11 +0330

    customizing the list page

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['author', 'text']
+    list_per_page = 5
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'bio']
+    list_per_page = 10
--- twitter/models.py
+++ twitter/models.py
@@ -9,9 +9,21 @@ class CustomUser(models.Model):
     bio = models.TextField()
     birthday = models.DateField(null=True)
 
+    def __str__(self) -> str:
+        return self.username
+    
+    class Meta:
+        ordering = ['username']
+
 class Tweet(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.author.username
+    
+    class Meta:
+        ordering = ['created_at']
